459c53de7ce5b39c9173078271e82f4db5f602e9
Tomasz Rarok <tomasz.rarok@gmail.com>
2021-01-10 22:26:26 +0100

    Configure Derby and Hibernate

--- src/main/java/pl/tomaszrarok/osm/MainApp.java
+++ src/main/java/pl/tomaszrarok/osm/MainApp.java
@@ -1,5 +1,9 @@
 package pl.tomaszrarok.osm;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -45,8 +49,31 @@ public class MainApp {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
+                initializeDatabase();
             }
         });
+    }
+
+    private static void initializeDatabase() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        Test test = em.find(Test.class, 1);
+        if (test == null) {
+            test = new Test();
+            test.id = 1;
+            test.data = "a";
+
+            tx.begin();
+            em.persist(test);
+            tx.commit();
+        }
+
+        System.out.format("Test{id=%s, data=%s}\n", test.id, test.data);
+
+        em.close();
+        emf.close();
     }
 
     private static void createAndShowGUI() {
---
+++ src/main/java/pl/tomaszrarok/osm/Test.java
@@ -0,0 +1,17 @@
+package pl.tomaszrarok.osm;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "test")
+public class Test {
+
+    @Id
+    public int id;
+
+    @Basic
+    public String data;
+}
